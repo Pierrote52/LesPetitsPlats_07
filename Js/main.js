ac3d10a746bee6e150c5bcafb0aec6f68b3083dc
Pierrote52 <peyalot@gmail.com>
2021-12-11 13:57:35 +0100

    First commit algo1

--- Js/main.js
+++ Js/main.js
@@ -6,26 +6,41 @@ var section = document.getElementById("section");
 //Liste des objets de la liste. 
 var listeRecipes = [];
 
+//CurentListe la liste. 
+
 for(let i=0; i<recipesArray.length; i++){
     var recipe = createRecipe(recipesArray[i][1]);
     listeRecipes.push(recipe);
 }
-//affiche les vignettes présentent dans la liste listeRecipes.
-initVignettes();
 
-function initVignettes(){
-    for(let i=0;i<listeRecipes.length;i++){
-        section.appendChild(listeRecipes[i].htmlContent());
+displayVignettes(listeRecipes);
+
+//affiche les vignettes présentent dans la liste liste.
+function displayVignettes(liste){
+    section.innerHTML="";
+    for(let i=0;i<liste.length;i++){
+        section.appendChild(liste[i].htmlContent());
     }
 }
 
 
+
+
+
+// Quand un element est modifier saisie dans la grande barre de recherche.
 value.addEventListener('keyup', function(v){
     //PLus de 3 caractères ? 
     if(v.target.value.length >=3){
         console.log(v.target.value);
-        
+        checkAllFiltersValue(v.target.value);
     }
 
 });
+
+//Regarde dans tout les filtres si la value envoyée dans la function 
+//est présente dans tous les filtres de la liste listeRecipes. 
+function checkAllFiltersValue(value){
+    
+
+}
 
